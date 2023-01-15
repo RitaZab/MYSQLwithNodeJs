cbe019af6bad1a5b5be257e757c8d600d4cc05a9
Rita Zaborowska <rm.zaborowska@gmail.com>
2023-01-15 21:03:36 +0100

    Adding more express functions

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import express from "express";
 import {getAllValues,getSingleValue,addingValue} from './database.js'
 const app = express()
 
+app.use(express.json())
+
 //These lines are normal error handling with is standard for express 5 beta
 app.use((err, req, res, next)=> {
     console.log(err.stack)
@@ -17,4 +19,17 @@ app.listen(8001, () => {
 app.get('/animals', async (req, res) => {
     const list=await getAllValues()
     res.send(list)
+})
+//Getting animal with specific id
+app.get('/animals/:id', async (req, res) => {
+    const id = req.params.id
+    const value = await getSingleValue(id)
+    res.send(value)
+})
+
+//Adding new value
+app.post('/animals', async (req, res) => {
+    const { animal_name, species } = req.body
+    const newValue = await addingValue(animal_name, species)
+    res.status(201).send(newValue)
 })
